8ba9d3acbaf4a6d6b74dfb561431e588c26fd171
Ian Lucas <ian.lucas@gmail.com>
2025-02-22 11:01:52 -0500

    feat: Set Auth Roles on Deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,20 +3,50 @@
 pragma solidity ^0.8.20;
 
 import {SimpleTokenizedStaker} from "../src/spikes/SimpleTokenizedStaker.sol";
-import {ERC20} from "@tokenized-strategy/BaseStrategy.sol";
+import {ITokenizedStaker} from "@periphery/Bases/Staker/ITokenizedStaker.sol";
 
+import {ERC20} from "@tokenized-strategy/BaseStrategy.sol";
+import {TomlConfig} from "@credbull-script/TomlConfig.s.sol";
+
+import { stdToml } from "forge-std/StdToml.sol";
 import { Script } from "forge-std/Script.sol";
 import { console2 } from "forge-std/console2.sol";
+import {TokenizedStaker} from "../lib/tokenized-strategy-periphery/src/Bases/Staker/TokenizedStaker.sol";
 
-contract Deploy is Script {
+contract Deploy is TomlConfig {
+    using stdToml for string;
+
+    string private _tomlConfig;
 
     ERC20 private asset;
+    YearnAuth private _yearnAuth;
 
     error UnknownChain(uint256 chainId);
+
+    struct YearnAuth {
+        address governance; // address that owns the contract (owner)
+        address emergencyAdmin; // can halt operators (owner)
+        address management; // can set fees, profit unlocking time etc (operator)
+        address keeper; // can report() to accrue p&l, charge fees, and lock profit to distribute (asset manager)
+
+        // strategy specific ?
+        address perfFeeRecipient; // perf fees are optional. (custodian)
+        // address protocolFeeRecipient; // fee on the perf fees, sent to yearn treasury
+    }
 
     // Mainnets:
     // Testnets: arbSepolia=421614
     constructor() {
+        _tomlConfig = loadTomlConfigFromLib();
+
+        _yearnAuth = YearnAuth({
+            governance: _tomlConfig.readAddress(".evm.address.owner"),
+            emergencyAdmin: _tomlConfig.readAddress(".evm.address.owner"),
+            management: _tomlConfig.readAddress(".evm.address.operator"),
+            keeper: _tomlConfig.readAddress(".evm.address.asset_manager"),
+            perfFeeRecipient: _tomlConfig.readAddress(".evm.address.custodian")
+        });
+
         if (block.chainid == getChain("anvil").chainId) {
             asset = ERC20(makeAddr("testToken"));
         }
@@ -29,38 +59,58 @@ contract Deploy is Script {
         }
     }
 
-    function run() public returns (SimpleTokenizedStaker tokenizedStaker_) {
+    function run() public returns (TokenizedStaker staker_) {
+        return run(_yearnAuth);
+    }
+
+    function run(YearnAuth memory yearnAuth) public returns (TokenizedStaker staker_) {
+        TokenizedStaker staker = _deployStaker();
+        _initStaker(staker, yearnAuth);
+
+        return staker;
+    }
+
+    function _deployStaker() internal returns (TokenizedStaker staker_) {
         vm.startBroadcast();
 
         // instantiate
-        SimpleTokenizedStaker tokenizedStaker = new SimpleTokenizedStaker(address(asset), "TestTokenStaker 20250221");
+        TokenizedStaker staker = new SimpleTokenizedStaker(address(asset), "TestTokenStaker 20250221");
 
         // log
         console2.log(
             string.concat(
                 "!!!!! Deploying SimpleTokenizedStaker [",
-                vm.toString(address(tokenizedStaker)),
+                vm.toString(address(staker)),
                 "] !!!!!"
             )
         );
 
         // TODO - set these as well...
-
-//        staker.setKeeper(keeper);
-//        staker.setPerformanceFeeRecipient(performanceFeeRecipient);
-//        staker.setPendingManagement(management);
-//        // Accept management.
-//        vm.prank(management);
-//        staker.acceptManagement();
-//
-//        // Add initial reward token
-//        vm.prank(management);
-//        staker.addReward(address(rewardToken), management, duration);
-//
-//        // TODO - separate protocolFeeRecipient out from governance.  in yearn-tok-strat-periph it is address(56)
-//        protocolFeeRecipient = IFactory(factory).governance();
         vm.stopBroadcast();
 
-        return tokenizedStaker;
+        return staker;
+    }
+
+    function _initStaker(TokenizedStaker stakerImpl, YearnAuth memory yearnAuth) public {
+
+        ITokenizedStaker staker = ITokenizedStaker(address(stakerImpl));
+
+        vm.startBroadcast();
+
+        staker.setKeeper(yearnAuth.keeper);
+        staker.setPerformanceFeeRecipient(yearnAuth.perfFeeRecipient);
+        staker.setPendingManagement(yearnAuth.management);
+        staker.setEmergencyAdmin(yearnAuth.emergencyAdmin);
+
+        vm.stopBroadcast();
+    }
+
+
+    function loadTomlConfigFromLib() internal view returns (string memory) {
+        string memory environment = vm.envString("ENVIRONMENT");
+
+        string memory path = string.concat(vm.projectRoot(), "/lib/credbull-defi/packages/contracts/resource/", environment, ".toml");
+        console2.log(string.concat("Loading toml configuration from: ", path));
+        return vm.readFile(path);
     }
 }
--- src/test/script/DeployTest.sol
+++ src/test/script/DeployTest.sol
@@ -10,13 +10,35 @@ contract DeployTest is Test {
 
     function test__Deploy__Deploy() public {
 
+        Deploy.YearnAuth memory yearnAuth = Deploy.YearnAuth({
+            governance: makeAddr("governance"), // not used in Strategy
+            emergencyAdmin: makeAddr("emergencyAdmin"),
+            management: makeAddr("management"),
+            keeper: makeAddr("keeper"),
+            perfFeeRecipient: makeAddr("perfFeeRecipient")
+        });
+
         Deploy deploy = new Deploy();
         ITokenizedStaker staker = ITokenizedStaker(
-            address(deploy.run())
+            address(deploy.run(yearnAuth))
         );
 
         assertNotEq(address(0), address(staker));
         assertNotEq(address(0), address(staker.asset()));
+
+        // check roles setup
+        // assertEq(staker.governance(), yearnAuth.governance, "governance not set"); // not on strategy
+        assertEq(staker.emergencyAdmin(), yearnAuth.emergencyAdmin, "emergencyAdmin not set");
+        assertEq(staker.pendingManagement(), yearnAuth.management, "pending management not set");
+        assertEq(staker.keeper(), yearnAuth.keeper, "keeper not set");
+        assertEq(staker.performanceFeeRecipient(), yearnAuth.perfFeeRecipient, "perfFeeRecipient not set");
+
+        // 2-step process for management
+        vm.prank(yearnAuth.management);
+        staker.acceptManagement();
+
+        assertEq(staker.management(), yearnAuth.management, "management not set");
+
     }
 
 }
